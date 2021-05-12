d123bde55fce8283e687bc410c4c0f3a0abf316b
mike-bskim <mike-bskim@gmail.com>
2021-05-12 17:11:32 +0200

    section 8.1 8.2

--- section 8/1, 2. 네트워크 선 자르기/aa.py
+++ section 8/1, 2. 네트워크 선 자르기/aa.py
@@ -1,17 +1,31 @@
 import sys
-# sys.stdin=open('input.txt', 'rt')
+sys.stdin=open('in3.txt', 'rt')
+
+def DFS(L):
+
+  # if a[L]>0:
+  #   return a[L]
+
+  if L==1 or L==2:
+    # a[L]=L
+    return L
+    # print(cnt)
+  else:
+    # print('>>>', L)
+    a[L]=DFS(L-1)+DFS(L-2)
+    # print('***a[{}] >> {}'.format(L, a[L]))
+    return a[L]
+
+  
 
 if __name__=='__main__':
   n = int(input())
+  cnt = 0
   # print(n)
 
   a=[0]*(n+1)
   # print(a)
+  cnt = DFS(n)
+  print(cnt)
 
-  a[1]=1
-  a[2]=2
-  for i in range(3, n+1):
-    a[i] = a[i-1] + a[i-2]
-    # print(i, '>>', a[i])
-  print(a[n])
   
