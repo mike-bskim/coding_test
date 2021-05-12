1f8dc9f599b3f9d33a53f9c19a72fe405149cffc
mike-bskim <mike-bskim@gmail.com>
2021-05-12 10:54:36 +0200

    병렬정렬

--- 섹션 7/병합정렬/aa.py
+++ 섹션 7/병합정렬/aa.py
@@ -1,3 +1,4 @@
+import random
 # 병합정렬
 def Dsort(lt, rt):
   if lt<rt:
@@ -22,12 +23,40 @@ def Dsort(lt, rt):
       arr[lt+i] = tmp[i]
 
 
+
+
+def DDsort(lt, rt):
+  if lt<rt:
+    mid=(lt+rt)//2
+    DDsort(lt, mid)
+    DDsort(mid+1, rt)
+    p1=lt
+    p2=mid+1
+    tmp=[]
+    while p1<=mid and p2<=rt:
+      if arr[p1]<arr[p2]:
+        tmp.append(arr[p1])
+        print('arr[{}]:{}, arr[{}]:{}, tmp:{}'.format(p1, arr[p1], p2, arr[p2], tmp))
+        p1+=1
+      else:
+        tmp.append(arr[p2])
+        print('arr[{}]:{}, arr[{}]:{}, tmp:{}'.format(p1, arr[p1], p2, arr[p2], tmp))
+        p2+=1
+    if p1<=mid:
+      tmp = tmp + arr[p1:mid+1]
+    if p2<=rt:
+      tmp = tmp + arr[p2:rt+1]
+    for i in range(len(tmp)):
+      arr[lt+i] = tmp[i]
+
+
 if __name__ == "__main__":
-  arr=[23,11,45,36,15,67,33,21]
+  arr=[random.randint(1,1000) for _ in range(8)]
+  # arr=[23,11,45,36,15,67,33,21]
   print('Before sort : ', end=' ')
   print(arr)
 
-  Dsort(0,7)
+  DDsort(0,len(arr)-1)
   print()
 
   print('After sort : ', end=' ')
